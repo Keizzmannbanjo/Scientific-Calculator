bd439684757ee4cd8a8aa07dc0a39943109a94eb
Keizzmannbanjo <keizzmannbanjo@gmail.com>
2021-08-04 15:26:38 +0100

    Added button click and display functionalities

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
-let calc_keys=document.querySelector('#keys');
+let calc_keys = document.querySelector("#keys");
+let textBox = document.querySelector("textarea");
 
+calc_keys.addEventListener("click", calcKeysClicked);
 
-
-calc_keys.addEventListener('click',calcKeysClicked);
-
-function calcKeysClicked(e){
-    e.preventDefault()
-    
+function calcKeysClicked(e) {
+  e.preventDefault();
+  if (e.target.tagName == "BUTTON") {
+    textBox.value += e.target.dataset.keyvalue;
+  }
 }
