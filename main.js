83954e9f1a132c77691aa1a9eafb63370b0f928c
Keizzmannbanjo <keizzmannbanjo@gmail.com>
2021-08-05 15:17:46 +0100

    Added trigonometric and other arithmetic operations

--- main.js
+++ main.js
@@ -14,13 +14,63 @@ clearBtn.addEventListener("click", clearinput);
 // * Solves user inputs operation
 function calculateInput(e) {
   let text = input.innerHTML;
+  // ? Handles division symbol
   text = text.replace(/\u00F7/g, "/");
+
+  // ? solves and replaces PI
   let p = Math.PI.toString();
   text = text.replace(/\u03C0/g, p);
-  //   let found = text.match(/\u221A\d+/g);
-  //   found.forEach((item) => {
-  //     let count = 0;
-  //   });
+
+  // ? Solves and replaces Square root operations
+  let found = text.match(/\u221A\d+/g);
+  if (found != null) {
+    found.forEach((item) => {
+      let num = item.substring(1, item.length);
+      let ans = Math.sqrt(num);
+      text = text.replace(item, ans.toString());
+    });
+  }
+
+  // ? Solves and replaces sin operation
+  found = text.match(/sin\d+/g);
+  if (found != null) {
+    found.forEach((item) => {
+      let num = item.substring(3, item.length);
+      let ans = Math.sin(num);
+      text = text.replace(item, ans.toString());
+    });
+  }
+
+  // ? Solves and replace cos operation
+  found = text.match(/cos\d+/g);
+  if (found != null) {
+    found.forEach((item) => {
+      let num = item.substring(3, item.length);
+      let ans = Math.cos(num);
+      text = text.replace(item, ans.toString());
+    });
+  }
+
+  // ? Solves and replace tan operation
+  found = text.match(/tan\d+/g);
+  if (found != null) {
+    found.forEach((item) => {
+      let num = item.substring(3, item.length);
+      let ans = Math.tan(num);
+      text = text.replace(item, ans.toString());
+    });
+  }
+
+  // ? Solves and replace log operation
+  found = text.match(/log\d+/g);
+  if (found != null) {
+    found.forEach((item) => {
+      let num = item.substring(3, item.length);
+      let ans = Math.log(num);
+      text = text.replace(item, ans.toString());
+    });
+  }
+
   result = eval(text);
   output.innerHTML += `\n\n\t${result}`;
   e.stopImmediatePropagation();
