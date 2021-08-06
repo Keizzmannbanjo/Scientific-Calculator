a30bb3015178d16db0536a063ba8c54461e59594
Keizzmannbanjo <keizzmannbanjo@gmail.com>
2021-08-06 15:04:30 +0100

    Bug fixes

--- main.js
+++ main.js
@@ -3,20 +3,30 @@ let calcKeys = document.querySelectorAll(".calc-key"),
     input = document.querySelector(".input"),
     output = document.querySelector(".output"),
     clearBtn = document.querySelector("#clearBtn"),
-    equalsBtn = document.querySelector("#equalsBtn")
+    equalsBtn = document.querySelector("#equalsBtn"),
+    backspaceBtn = document.querySelector("#backspaceBtn")
 
 calcKeys.forEach((item) => {
     item.addEventListener("click", calcKeysClicked)
 })
-
 clearBtn.addEventListener("click", clearinput)
+// backspaceBtn.addEventListener("click", deleteInput)
 
 // * Solves user inputs operation
 function calculateInput(e) {
-    let text = input.innerHTML
+    let text = input.value
+    if (text == "" || text == undefined || text == null) {
+        output.innerHTML = "Math Error"
+        return
+    }
+
     // ? Handles division symbol
     text = text.replace(/\u00F7/g, "/")
 
+    // ? Handles euler's number symbol
+    text = text.replace(/\u2107/g, "2.17828")
+
+    // ? Handles power function
     text = text.replace(/\^/g, "**")
 
     // ? solves and replaces PI
@@ -33,21 +43,30 @@ function calculateInput(e) {
         })
     }
 
+    found = text.match(/\d+\%/g)
+    if (found != null) {
+        found.forEach((item) => {
+            let num = Number(item.substring(0, item.length - 1))
+            num = num / 100
+            text = text.replace(item, num.toString())
+        })
+    }
+
     // ? Solves and replaces sin or asin operation
-  found = text.match(/(sin\d+)|(asin\d+)/g)
-  if (found != null) {
-      found.forEach((item) => {
-          let num, ans
-          if (item.substr(0, 4) == "asin") {
-              num = item.substring(4, item.length)
-              ans = 1 / Math.sin(num)
-          } else {
-              num = item.substring(3, item.length)
-              ans = Math.sin(num)
-          }
-          text = text.replace(item, ans.toString())
-      })
-  }
+    found = text.match(/(sin\d+)|(asin\d+)/g)
+    if (found != null) {
+        found.forEach((item) => {
+            let num, ans
+            if (item.substr(0, 4) == "asin") {
+                num = item.substring(4, item.length)
+                ans = 1 / Math.sin(num)
+            } else {
+                num = item.substring(3, item.length)
+                ans = Math.sin(num)
+            }
+            text = text.replace(item, ans.toString())
+        })
+    }
 
     // ? Solves and replace cos or acos operation
     found = text.match(/(cos\d+)|(acos\d+)/g)
@@ -56,7 +75,7 @@ function calculateInput(e) {
             let num, ans
             if (item.substr(0, 4) == "acos") {
                 num = item.substring(4, item.length)
-                ans = 1 / (Math.cos(num))
+                ans = 1 / Math.cos(num)
             } else {
                 num = item.substring(3, item.length)
                 ans = Math.cos(num)
@@ -66,20 +85,20 @@ function calculateInput(e) {
     }
 
     // ? Solves and replace tan or atan operation
-   found = text.match(/(tan\d+)|(atan\d+)/g)
-   if (found != null) {
-       found.forEach((item) => {
-           let num, ans
-           if (item.substr(0, 4) == "atan") {
-               num = item.substring(4, item.length)
-               ans = 1 / Math.tan(num)
-           } else {
-               num = item.substring(3, item.length)
-               ans = Math.tan(num)
-           }
-           text = text.replace(item, ans.toString())
-       })
-   }
+    found = text.match(/(tan\d+)|(atan\d+)/g)
+    if (found != null) {
+        found.forEach((item) => {
+            let num, ans
+            if (item.substr(0, 4) == "atan") {
+                num = item.substring(4, item.length)
+                ans = 1 / Math.tan(num)
+            } else {
+                num = item.substring(3, item.length)
+                ans = Math.tan(num)
+            }
+            text = text.replace(item, ans.toString())
+        })
+    }
 
     // ? Solves and replace log operation
     found = text.match(/log\d+/g)
@@ -90,7 +109,6 @@ function calculateInput(e) {
             text = text.replace(item, ans.toString())
         })
     }
-    console.log((3673).toString(16))
 
     // ? Solves and replace hexadecimal numbers
     found = text.match(/hex\d+/g)
@@ -103,8 +121,13 @@ function calculateInput(e) {
         return
     }
 
-    result = eval(text)
-    output.innerHTML += `\n\n\t${result}`
+    try {
+        result = eval(text)
+        output.innerHTML += `\n\n\t${result}`
+    } catch (error) {
+        input.value = ""
+        output.innerHTML = "Math Error"
+    }
     e.stopImmediatePropagation()
 }
 
@@ -114,12 +137,12 @@ function calcKeysClicked(e) {
         output.innerHTML = ""
         calculateInput(e)
     } else {
-        input.innerHTML += e.target.dataset.keyvalue
+        input.value += e.target.dataset.keyvalue
     }
 }
 
 // * Clears the screen
 function clearinput(e) {
-    input.innerHTML = ""
+    input.value = ""
     output.innerHTML = ""
 }
